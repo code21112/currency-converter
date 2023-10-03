eb84cde7c2d1c25319baf87a6be48d2b430e68df
code21112 <code21112@protonmail.com>
2023-10-03 17:12:15 +0200

    Controlled input and selects

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
+import Input from "./components/Input";
+
 function App() {
   return (
     <div>
-      <input type="text" />
-      <select>
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-        <option value="CAD">CAD</option>
-        <option value="INR">INR</option>
-      </select>
-      <select>
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-        <option value="CAD">CAD</option>
-        <option value="INR">INR</option>
-      </select>
-      <p>OUTPUT</p>
+      <Input />
     </div>
   );
 }
