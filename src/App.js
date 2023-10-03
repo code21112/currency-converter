364a4335c214c1ab613fb2e881284f330906ab3b
code21112 <code21112@protonmail.com>
2023-10-03 16:11:41 +0200

    Created components folder and splitted code with Input component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Input from "./components/Input";
 
 function App() {
   return (
     <div>
-      <input type="text" />
-      <select>
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-        <option value="CAD">CAD</option>
-        <option value="INR">INR</option>
-      </select>
-      <select>
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-        <option value="CAD">CAD</option>
-        <option value="INR">INR</option>
-      </select>
-      <p>OUTPUT</p>
+      <Input />
     </div>
   );
 }
